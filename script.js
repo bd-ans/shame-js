fd8fce7397bb057829cf2476d6cd03781e759212
Elayt <elaytpatters@gmail.com>
2022-07-28 16:01:28 +0500

    .

---
+++ script.js
@@ -0,0 +1,44 @@
+var nameMsg = `Iltimos, ismingizni kiriting!`;
+var welcomeMsg = `Hush kelibsiz`;
+var confirmMsg = `Savollarimizga javob berishga tayyormisiz?`
+var addresQuestion = `Yashash manzilingiz?`;
+var secondQuestion = `Yoshizgiz? (sonda)`;
+var byeMsg = `Javoblar uchun rahmat!`;
+var ignoreMsg = `Siz savollarimizga javob berishni hohlamadingiz (`;
+var retryMsg = `iltimos ma'lmotlarningizni to'liq kiriting!`;
+
+var reloadPage = function() {
+  setTimeout("location.reload(true);",500); // Reload the page
+}
+
+nameMsg = prompt(nameMsg).trim();
+
+if (nameMsg === '' || nameMsg === null || nameMsg === undefined) {
+  alert(`${retryMsg}`);
+  reloadPage();
+} else {
+  alert(welcomeMsg + `, ${nameMsg}`);
+  confirmMsg = confirm(confirmMsg);
+  if (confirmMsg === false) {
+    alert(ignoreMsg);
+  } else {
+    addresQuestion = prompt(addresQuestion);
+    if (addresQuestion === '' || addresQuestion === null || addresQuestion === undefined) {
+      alert(`${retryMsg}`);
+      reloadPage();
+    } else {
+      secondQuestion = prompt(secondQuestion);
+      if (secondQuestion === '' || secondQuestion === null || secondQuestion === undefined) {
+        alert(`${retryMsg}`);
+        reloadPage();
+      } else {
+      byeMsg = alert(byeMsg + `, ${nameMsg}`);
+      console.log(`Foydalanuvchi ismi: ${nameMsg}
+
+Foydalanuvchi yashash manzili: ${addresQuestion}
+
+Foydalanuvchi yoshi: ${secondQuestion}`);
+      }
+    }
+  }
+}
